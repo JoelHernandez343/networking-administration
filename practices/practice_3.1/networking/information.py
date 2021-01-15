098e57e6cb70b053e1374bdee05f2921ca2c1394
JoelHernandez343 <joel.programador@gmail.com>
2021-01-15 01:03:20 -0600

    Modified get_sys_info

--- practices/practice_3.1/networking/information.py
+++ practices/practice_3.1/networking/information.py
@@ -1,4 +1,4 @@
-from networking.snmp import snmp_query
+from networking.snmp import snmp_query, oids
 
 community = "ro_4CM1"
 
@@ -6,9 +6,10 @@ community = "ro_4CM1"
 def get_sys_info(ip):
     info = {}
 
-    info["sysDescr"] = snmp_query(ip, community, "1.3.6.1.2.1.1.1.0")
-    info["sysContact"] = snmp_query(ip, community, "1.3.6.1.2.1.1.4.0")
-    info["sysName"] = snmp_query(ip, community, "1.3.6.1.2.1.1.5.0")
-    info["sysLocation"] = snmp_query(ip, community, "1.3.6.1.2.1.1.6.0")
+    info["sysDescr"] = snmp_query(ip, community, oids.DESCR_OID)
+    info["sysContact"] = snmp_query(ip, community, oids.CONTACT_OID)
+    info["sysName"] = snmp_query(ip, community, oids.HOSTNAME_OID)
+    info["sysLocation"] = snmp_query(ip, community, oids.LOCATION_OID)
+    info["hostname"] = info["sysName"].split(".")[0]
 
     return info
