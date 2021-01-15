bcb1a3e408cf04e2f8339776cd23325e5d6e4ee9
JoelHernandez343 <joel.programador@gmail.com>
2021-01-15 14:45:55 -0600

    Improvement in some information.py functions

--- practices/practice_3.1/networking/snmp/information.py
+++ practices/practice_3.1/networking/snmp/information.py
@@ -4,46 +4,43 @@ community = "ro_4CM1"
 
 
 def get_sys_info(ip):
-    info = {}
+    info = {
+        "sysDescr": snmp_query(ip, community, oids.DESCR_OID),
+        "sysContact": snmp_query(ip, community, oids.CONTACT_OID),
+        "sysName": snmp_query(ip, community, oids.HOSTNAME_OID),
+        "sysLocation": snmp_query(ip, community, oids.LOCATION_OID),
+    }
 
-    info["sysDescr"] = snmp_query(ip, community, oids.DESCR_OID)
-    info["sysContact"] = snmp_query(ip, community, oids.CONTACT_OID)
-    info["sysName"] = snmp_query(ip, community, oids.HOSTNAME_OID)
-    info["sysLocation"] = snmp_query(ip, community, oids.LOCATION_OID)
     info["hostname"] = info["sysName"].split(".")[0]
 
     return info
 
 
 def get_if_inout(ip, n):
-    info = {}
-
-    info["ifInOctets"] = snmp_query(ip, community, f"{oids.INTERFACE_OID}.10.{n}")
-    info["ifOutOctets"] = snmp_query(ip, community, f"{oids.INTERFACE_OID}.16.{n}")
-
-    info["ifInUcastPkts"] = snmp_query(ip, community, f"{oids.INTERFACE_OID}.11.{n}")
-    info["ifOutUcastPkts"] = snmp_query(ip, community, f"{oids.INTERFACE_OID}.17.{n}")
-
-    return info
+    return {
+        "ifInOctets": snmp_query(ip, community, f"{oids.INTERFACE_OID}.10.{n}"),
+        "ifOutOctets": snmp_query(ip, community, f"{oids.INTERFACE_OID}.16.{n}"),
+        "ifInUcastPkts": snmp_query(ip, community, f"{oids.INTERFACE_OID}.11.{n}"),
+        "ifOutUcastPkts": snmp_query(ip, community, f"{oids.INTERFACE_OID}.17.{n}"),
+    }
 
 
 def get_if_info(ip, n):
-    info = {}
 
-    info["ifDescr"] = snmp_query(ip, community, f"{oids.INTERFACE_OID}.2.{n}")
-    info["ifMtu"] = snmp_query(ip, community, f"{oids.INTERFACE_OID}.4.{n}")
-    info["ifSpeed"] = snmp_query(ip, community, f"{oids.INTERFACE_OID}.5.{n}")
-    info["ifPhysAddress"] = convert.mac(
-        snmp_query(ip, community, f"{oids.INTERFACE_OID}.6.{n}")
-    )
-    info["ifAdminSystem"] = convert.status(
-        snmp_query(ip, community, f"{oids.INTERFACE_OID}.7.{n}")
-    )
-    info["ifOperStatus"] = convert.status(
-        snmp_query(ip, community, f"{oids.INTERFACE_OID}.8.{n}")
-    )
-
-    return info
+    return {
+        "ifDescr": snmp_query(ip, community, f"{oids.INTERFACE_OID}.2.{n}"),
+        "ifMtu": snmp_query(ip, community, f"{oids.INTERFACE_OID}.4.{n}"),
+        "ifSpeed": snmp_query(ip, community, f"{oids.INTERFACE_OID}.5.{n}"),
+        "ifPhysAddress": convert.mac(
+            snmp_query(ip, community, f"{oids.INTERFACE_OID}.6.{n}")
+        ),
+        "ifAdminSystem": convert.status(
+            snmp_query(ip, community, f"{oids.INTERFACE_OID}.7.{n}")
+        ),
+        "ifOperStatus": convert.status(
+            snmp_query(ip, community, f"{oids.INTERFACE_OID}.8.{n}")
+        ),
+    }
 
 
 def get_interfaces(ip):
