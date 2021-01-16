7080bcf6520920697ad5533a4ba4c0d5ed04467c
JoelHernandez343 <joel.programador@gmail.com>
2021-01-15 22:11:51 -0600

    Added restore file for ssh pub keys

---
+++ practices/practice_3.1/restore_ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ssh-keygen -R 192.168.0.1
+ssh-keygen -R 192.168.0.2
+ssh-keygen -R 192.168.0.161
+ssh-keygen -R 192.168.0.162
+ssh-keygen -R 192.168.0.165
+ssh-keygen -R 192.168.0.166
+ssh-keygen -R 192.168.0.169
+ssh-keygen -R 192.168.0.170
+ssh-keygen -R 192.168.0.97
+ssh-keygen -R 192.168.0.33
+ssh-keygen -R 192.168.0.173
+ssh-keygen -R 192.168.0.174
+ssh-keygen -R 192.168.0.177
+ssh-keygen -R 192.168.0.178
+ssh-keygen -R 192.168.0.65
+ssh-keygen -R 192.168.0.129
